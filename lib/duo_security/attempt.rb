1b72b429beadfa36f7e07f07e22b5fe1f7b58cdf
Marten Veldthuis <marten@veldthuis.com>
2012-09-05 11:20:00 +0200

    Add implementation of Attempt

--- lib/duo_security/attempt.rb
+++ lib/duo_security/attempt.rb
@@ -1,11 +1,17 @@
 module DuoSecurity
   class Attempt
-    def initialize(username)
+    def initialize(api, username)
+      @api      = api
       @username = username
     end
 
     def login!
-      true
+      preauth = @api.preauth(@username)
+      factor  = preauth["factors"].fetch("default")
+      
+      @api.auth(@username, "auto", {"auto" => factor})
+    rescue API::UnknownUser => e
+      false
     end
   end
 end
--- spec/duo_security/attempt_spec.rb
+++ spec/duo_security/attempt_spec.rb
@@ -3,12 +3,26 @@ require_relative '../../lib/duo_security/attempt'
 
 module DuoSecurity
   describe Attempt do
+    let(:api) { API.new(ENV["DUO_HOST"], ENV["DUO_SKEY"], ENV["DUO_IKEY"]) }
+
     describe 'when using push notifications' do
       it 'returns true if the user accepts the login' do
-        Attempt.new("marten").login!.must_equal true
+        VCR.use_cassette("attempt_allowed") do
+          Attempt.new(api, "marten").login!.must_equal true
+        end
       end
 
-      it 'returns false if the user denies the login'
+      it 'returns false if the user denies the login' do
+        VCR.use_cassette("attempt_disallowed") do
+          Attempt.new(api, "marten").login!.must_equal false
+        end
+      end
+
+      it 'returns false if the user is not known' do
+        VCR.use_cassette("attempt_user_unknown") do
+          Attempt.new(api, "unknown").login!.must_equal false
+        end
+      end
     end
   end
 end
