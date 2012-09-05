0273eba06ee84dc739e409b379bfcd1121ef3a5a
Marten Veldthuis <marten@veldthuis.com>
2012-09-05 11:19:01 +0200

    Add message during running of tests about what to do on your phone.

--- spec/duo_security/api_spec.rb
+++ spec/duo_security/api_spec.rb
@@ -6,6 +6,9 @@ VCR.configure do |c|
   c.cassette_library_dir = "fixtures/vcr"
   c.hook_into :webmock
   c.allow_http_connections_when_no_cassette = true
+  c.before_http_request(:real?) do |request|
+    puts "Cassette #{VCR.current_cassette.name} being recorded. Take appropriate actions on your phone."
+  end
 end
 
 module DuoSecurity
@@ -16,7 +19,7 @@ module DuoSecurity
 
     describe '#ping' do
       it 'succeeds' do
-        VCR.use_cassette("ping_success") do
+        VCR.use_cassette("api_ping_success") do
           duo = API.new(host, skey, ikey)
           duo.ping.must_equal true
         end
@@ -25,21 +28,21 @@ module DuoSecurity
 
     describe '#check' do
       it 'succeeds with correct credentials' do
-        VCR.use_cassette("check_success") do
+        VCR.use_cassette("api_check_success") do
           duo = API.new(host, skey, ikey)
           duo.check.must_equal true
         end
       end
 
       it 'fails with incorrect skey' do
-        VCR.use_cassette("check_wrong_skey") do
+        VCR.use_cassette("api_check_wrong_skey") do
           duo = API.new(host, "wrong", ikey)
           duo.check.must_equal false
         end
       end
 
       it 'fails with incorrect ikey' do
-        VCR.use_cassette("check_wrong_ikey") do
+        VCR.use_cassette("api_check_wrong_ikey") do
           duo = API.new(host, skey, "wrong")
           duo.check.must_equal false
         end
@@ -48,7 +51,7 @@ module DuoSecurity
 
     describe '#preauth' do
       it 'returns a list of possible factors' do
-        VCR.use_cassette("preauth") do
+        VCR.use_cassette("api_preauth") do
           duo = API.new(host, skey, ikey)
           result = duo.preauth("marten")
           result["factors"].must_equal({"1"=>"push1", "2"=>"sms1", "default"=>"push1"})
@@ -61,24 +64,21 @@ module DuoSecurity
       let(:duo) { API.new(host, skey, ikey) }
 
       it 'returns true if user OKs the request' do
-        #puts 'Allow this request'
-        VCR.use_cassette("auth_user_accepts") do
+        VCR.use_cassette("api_auth_user_accepts") do
           result = duo.auth("marten", "push", "phone" => "phone1")
           result.must_equal(true)
         end
       end
 
       it 'returns false if the user denies the request as a mistake' do
-        #puts 'Disallow this request, say it was a mistake'
-        VCR.use_cassette("auth_user_denies_mistake") do
+        VCR.use_cassette("api_auth_user_denies_mistake") do
           result = duo.auth("marten", "push", "phone" => "phone1")
           result.must_equal(false)
         end
       end
 
       it 'returns false if the user denies the request as a fraudulent attack' do
-        #puts 'Disallow this request, say it was fraud'
-        VCR.use_cassette("auth_user_denies_fraud") do
+        VCR.use_cassette("api_auth_user_denies_fraud") do
           result = duo.auth("marten", "push", "phone" => "phone1")
           result.must_equal(false)
         end
